0c4a156ee7cf7561a6e9ac7aecba7cd83a5cd192
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-06-14 18:49:08 +0300

    Add missing content column in comments table

---
+++ db/migrate/20250614154758_add_content_to_comments.rb
@@ -0,0 +1,5 @@
+class AddContentToComments < ActiveRecord::Migration[8.0]
+  def change
+    add_column :comments, :content, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_14_153733) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_14_154758) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "content"
   end
 
   create_table "posts", force: :cascade do |t|

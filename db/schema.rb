26cd3d72abe893f93f59be7af9318d8cfe9fc602
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-06-14 17:03:26 +0300

    Migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_14_111954) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_14_140048) do
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.datetime "created_at", null: false

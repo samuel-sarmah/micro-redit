4e47987245c56065c1737cd591d5dc6d77226240
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-06-14 17:44:50 +0300

    Migrate to add user_id column to database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_14_140048) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_14_143827) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
